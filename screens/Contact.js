178009bed0d21273c97293c584129a1e894fa4ed
Ritik Jain <ritikjain1272@gmail.com>
2021-10-21 11:56:58 +0530

    Create Contact.js

---
+++ screens/Contact.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const Contact = () => {
+  return (
+    <View><Text>Contact Component</Text></View>
+    );
+}
+
+export default Contact;
